4775e835de78d64caf92eb60ee7a2edc6cefb9f0
Ben Askren <ben@buildcalc.com>
2024-10-20 09:13:01 -0400

    INITIAL COMMIT

---
+++ UnoApp3/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+namespace UnoApp3;
+
+public sealed partial class MainPage : Page
+{
+    
+    readonly Image Image1 = new()
+    {
+        Stretch = Microsoft.UI.Xaml.Media.Stretch.UniformToFill,
+        HorizontalAlignment = HorizontalAlignment.Stretch,
+        VerticalAlignment = VerticalAlignment.Stretch,
+    };
+    readonly Image Image2 = new()
+    {
+        Stretch = Microsoft.UI.Xaml.Media.Stretch.UniformToFill,
+        HorizontalAlignment = HorizontalAlignment.Stretch,
+        VerticalAlignment = VerticalAlignment.Stretch,
+    };
+
+    public MainPage()
+    {
+        this.InitializeComponent();
+        
+        StackPanel.Children.Add(new TextBlock{Text="Image1"});
+        var source1 = new Microsoft.UI.Xaml.Media.Imaging.SvgImageSource(new Uri("ms-appx:///Assets/Svg/awc_320x50_ccwd.svg"));
+        Image1.Source = source1;
+        StackPanel.Children.Add(Image1);
+        
+        StackPanel.Children.Add(new TextBlock{Text="Image2"});
+        var source2 = new Microsoft.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:///Assets/Ads/awc_320x50_ccwd.png"));
+        Image2.Source = source1;
+        StackPanel.Children.Add(Image2);
+        
+        
+    }
+}
