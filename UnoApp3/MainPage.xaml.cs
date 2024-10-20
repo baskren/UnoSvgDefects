0ecc0b6a029f1de5a6b7224309567a549a4a1b6f
Ben Askren <ben@buildcalc.com>
2024-10-20 11:05:30 -0400

    REFACTOR : Image labels

--- UnoApp3/MainPage.xaml.cs
+++ UnoApp3/MainPage.xaml.cs
@@ -20,17 +20,17 @@ public sealed partial class MainPage : Page
     {
         this.InitializeComponent();
         
-        StackPanel.Children.Add(new TextBlock{Text="Image1"});
+        StackPanel.Children.Add(new TextBlock{Text="Imaging.SvgImageSource"});
         var source1 = new Microsoft.UI.Xaml.Media.Imaging.SvgImageSource(new Uri("ms-appx:///Assets/Svg/awc_320x50_ccwd.svg"));
         Image1.Source = source1;
         StackPanel.Children.Add(Image1);
         
-        StackPanel.Children.Add(new TextBlock{Text="Image2"});
+        StackPanel.Children.Add(new TextBlock{Text="Imaging.BitmapImage (Resize-atizer)"});
         var source2 = new Microsoft.UI.Xaml.Media.Imaging.BitmapImage(new Uri("ms-appx:///Assets/Ads/awc_320x50_ccwd.png"));
         Image2.Source = source1;
         StackPanel.Children.Add(Image2);
         
-        StackPanel.Children.Add(new TextBlock{Text="Image3"});
+        StackPanel.Children.Add(new TextBlock{Text="Work around"});
         var svgImage = new EmbeddedSvgImage("UnoApp3.Resources.awc_320x50_ccwd.svg", GetType().Assembly)
         {
             Stretch = Microsoft.UI.Xaml.Media.Stretch.Uniform,
